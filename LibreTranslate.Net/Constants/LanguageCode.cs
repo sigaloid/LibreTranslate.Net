64b83d27f657880d3febb8ef56285c8f5c798e6f
Tobit Jelloneck <tobit-jelloneck@web.de>
2023-06-06 11:41:59 +0200

    Adding support for additional languages
    It now supports the following additional languages:
    -Azerbaijani
    -Catalan
    -Czech
    -Danish
    -Dutch
    -Esperanto
    -Finnish
    -Greek
    -Hebrew
    -Hungarian
    -Indonesia
    -Persian
    -Polish
    -Slovak
    -Swedish
    -Turkish
    -Ukranian

--- LibreTranslate.Net/Constants/LanguageCode.cs
+++ LibreTranslate.Net/Constants/LanguageCode.cs
@@ -35,17 +35,34 @@ namespace LibreTranslate.Net
         }
         public static readonly LanguageCode English = new LanguageCode("en");
         public static readonly LanguageCode Arabic = new LanguageCode("ar");
+        public static readonly LanguageCode Azerbaijani = new LanguageCode("az");
+        public static readonly LanguageCode Catalan = new LanguageCode("ca");
         public static readonly LanguageCode Chinese = new LanguageCode("zh");
+        public static readonly LanguageCode Czech = new LanguageCode("cs");
+        public static readonly LanguageCode Danish = new LanguageCode("da");
+        public static readonly LanguageCode Dutch = new LanguageCode("nl");
+        public static readonly LanguageCode Esperanto = new LanguageCode("eo");
+        public static readonly LanguageCode Finnish = new LanguageCode("fi");
         public static readonly LanguageCode French = new LanguageCode("fr");
         public static readonly LanguageCode German = new LanguageCode("de");
+        public static readonly LanguageCode Greek = new LanguageCode("el");
+        public static readonly LanguageCode Hebrew = new LanguageCode("he");
         public static readonly LanguageCode Hindi = new LanguageCode("hi");
+        public static readonly LanguageCode Hungarian = new LanguageCode("hu");
+        public static readonly LanguageCode Indonesian = new LanguageCode("id");
         public static readonly LanguageCode Irish = new LanguageCode("ga");
         public static readonly LanguageCode Italian = new LanguageCode("it");
         public static readonly LanguageCode Japanese = new LanguageCode("ja");
         public static readonly LanguageCode Korean = new LanguageCode("ko");
+        public static readonly LanguageCode Persian = new LanguageCode("fa");
+        public static readonly LanguageCode Polish = new LanguageCode("pl");
         public static readonly LanguageCode Portuguese = new LanguageCode("pt");
         public static readonly LanguageCode Russian = new LanguageCode("ru");
+        public static readonly LanguageCode Slovak = new LanguageCode("sk");
         public static readonly LanguageCode Spanish = new LanguageCode("es");
+        public static readonly LanguageCode Swedish = new LanguageCode("sv");
+        public static readonly LanguageCode Turkish = new LanguageCode("tr");
+        public static readonly LanguageCode Ukranian = new LanguageCode("uk");
         public static readonly LanguageCode AutoDetect = new LanguageCode("auto");
     }
 }
